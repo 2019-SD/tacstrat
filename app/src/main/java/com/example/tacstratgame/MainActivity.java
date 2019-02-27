17148218f3d441d52ac73abc6fa04bf6165b1834
Brendan Beecham <blbeecha@mtu.edu>
2019-02-27 15:48:31 -0500

    Implements menu for game and connects start button to initialization of the game tile board. Also changes tile names to be in-convention with Java.

--- app/src/main/java/com/example/tacstratgame/MainActivity.java
+++ app/src/main/java/com/example/tacstratgame/MainActivity.java
@@ -2,17 +2,29 @@ package com.example.tacstratgame;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Window;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(new GameView(this));
+
+        setContentView(R.layout.activity_main);
+        Button start = findViewById(R.id.button);
+
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    setContentView( new GameView(v.getContext()) );
+                    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+            }
+        });
+
     }
 }
