17300abfb1d5157b32db066cfc74f6b70357e7f7
isappleb <46874330+isappleb@users.noreply.github.com>
2019-04-17 13:35:12 -0400

    Add files via upload

---
+++ GameView.java
@@ -0,0 +1,147 @@
+package com.example.tacstratgame;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+import android.view.MotionEvent;
+import android.content.res.Resources;
+import android.view.View;
+
+
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+
+    private float screenWidth = (float) Resources.getSystem().getDisplayMetrics().widthPixels;
+    private float screenHeight = (float) Resources.getSystem().getDisplayMetrics().heightPixels;
+
+    private MainThread mainThread;
+    private Map map;
+    private CommandMenu commandMenu;
+    private Game game;
+    private MenuSet menuSet;
+    private static int turn = 1;
+
+    public GameView(Context context, Game game, MenuSet menuSet) {
+        super(context);
+
+        getHolder().addCallback(this);
+        this.game = game;
+        this.menuSet = menuSet;
+        map = new Map(1, getResources());
+        this.menuSet.setMap(map);
+        mainThread = new MainThread(game, this, menuSet);
+        setFocusable(true);
+        float x0 = screenWidth / 3;
+        float y0 = screenHeight - (screenHeight / 4);
+        commandMenu = new CommandMenu(x0, y0, 0, screenHeight);
+
+    }
+
+    public static int getTurn() { return turn; }
+
+    public static void setTurn(int turn) {
+        GameView.turn = turn;
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    public void update() {
+        map.update();
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        mainThread.setRunning(true);
+        mainThread.start();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        boolean retry = true;
+        while (retry) {
+            try {
+                mainThread.setRunning(false);
+                mainThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            retry = false;
+        }
+    }
+
+
+    /*
+     * UI Elements start here. Work in progress.
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        //Gets input details from the touch screen when activity is detected.
+        //Use ACTION_DOWN for a tap.
+        //Still need to detect collision.
+
+        float x = e.getX();
+        float y = e.getY();
+
+        //Do different things depending on action of MotionEvent.
+        switch(e.getAction()) {
+            //When user first touches the screen.
+            case MotionEvent.ACTION_DOWN:
+                menuSet.setActionDrawValue(false);
+                menuSet.setEnemyDrawValue(false);
+                menuSet.setPlayerDrawValue(false);
+
+                Tile tile = map.getTile(x,y);
+                menuSet.setTile(tile);
+                if (tile == null){
+                    map.stopDrawingMove();
+                } else {
+                    menuSet.updateTileInfo(tile);
+                    menuSet.updatePlayerDisplay(tile.getUnit());
+                    if(map.drawingMove()){
+                        if (map.canMove(map.getX(x),map.getY(y))){
+                            map.moveUnit(map.getX(x), map.getY(y));
+                            menuSet.setTileDrawValue(false);
+                            menuSet.updateVisibility();
+                            game.invalidate();
+                            return true;
+                        } else{
+                            map.stopDrawingMove();
+                        }
+                    } else if( map.drawingAttack()){
+                        map.attack(map.getX(x), map.getY(y));
+                        menuSet.setTileDrawValue(false);
+                        menuSet.updateVisibility();
+                        game.invalidate();
+                        return true;
+                    } else if( map.drawingSpecial()){
+                        map.special(map.getX(x), map.getY(y));
+                        menuSet.setTileDrawValue(false);
+                        menuSet.updateVisibility();
+                        game.invalidate();
+                        return true;
+                    }
+                    else if(tile.hasUnit()){
+                        menuSet.setActionDrawValue(true);
+                        menuSet.setPlayerDrawValue(true);
+                    }
+                }
+
+                menuSet.updateVisibility();
+                game.invalidate();
+
+        }
+
+        return true;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        map.draw(canvas);
+    }
+
+
+}
---
+++ Marksman.java
@@ -0,0 +1,87 @@
+package com.example.tacstratgame;
+
+public class Marksman implements Unit {
+
+    private int attack = 75;
+    private int defense = 25;
+    private int hp = 200;
+    private int hpMax = 200;
+    private int mvmt = 1;
+    private int range = 3;
+    private int x;
+    private int y;
+    private int team;
+    private int hasMoved;
+    private int hasAttacked;
+    private int hasDefended;
+    private int image = R.drawable.cowboy;
+    private String name = "Marksman";
+
+    public Marksman(int x, int y, int team){
+        this.x = x;
+        this.y = y;
+        this.team = team;
+        hasMoved = 0;
+        hasAttacked = 0;
+        hasDefended = 0;
+    }
+
+    @Override
+    public int getHasAttacked() { return hasAttacked; }
+    public int getHasDefended() { return hasDefended; }
+    public int getHasMoved() { return hasMoved; }
+    public int getTeam() { return team; }
+    public int getAttack() {
+        return attack;
+    }
+    public int getDefense(){
+        return defense;
+    }
+    public int getHp() {
+        return hp;
+    }
+    public int getHpMax() { return hpMax; }
+    public int getMvmt(){
+        return mvmt;
+    }
+    public int getRange(){
+        return range;
+    }
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public int getImage(){ return image; }
+    public String getName() { return name; }
+
+    public void setHasAttacked(int hasAttacked) { this.hasAttacked = hasAttacked; }
+    public void setHasDefended(int hasDefended) { this.hasDefended = hasDefended; }
+    public void setHasMoved(int hasMoved) { this.hasMoved = hasMoved; }
+    public void setAttack(int attack){
+        this.attack = attack;
+    }
+    public void setDefense(int defense){
+        this.defense = defense;
+    }
+    public void setHp(int hp){
+        this.hp = hp;
+    }
+    public void setHpMax(int hpMax) { this.hpMax = hpMax; }
+    public void setMvmt(int mvmt){
+        this.mvmt = mvmt;
+    }
+    public void setRange(int range){
+        this.range = range;
+    }
+    public void setX(int x) { this.x = x; }
+    public void setY(int y) { this.y = y; }
+    public void setName(String name) { this.name = name; }
+
+    public void printStats( ) {
+        System.out.printf( "Marksman Stats:\n Attack: %d\n Defense: %d\n Health Points: %d\n Movement: %d\n Attack Range: %d\n", attack, defense, hp, mvmt, range );
+
+    }
+
+    public static void main( String []args ) {
+        Marksman mark = new Marksman(0,0, 1);
+        mark.printStats();
+    }
+}
